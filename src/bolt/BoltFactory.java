bd5971f24639738a7f895734a0b5343fc491c7cd
Nikolett Uracs <loveninagold@gmail.com>
2017-02-26 19:29:34 +0100

    implemented task 5.1

--- src/bolt/BoltFactory.java
+++ src/bolt/BoltFactory.java
@@ -1,12 +1,18 @@
 package bolt;
 
+import bolt.aruk.Sajt;
+import bolt.aruk.Szappan;
+import bolt.aruk.tej.FeltartosTej;
+import bolt.aruk.tej.TartosTej;
+import.bolt.aruk.Tej;
+
 import java.util.Date;
 
 import bolt.aruk.Tej;
 import bolt.aruk.tej.FeltartosTej;
 import bolt.aruk.tej.TartosTej;
 
-public abstract class TejFactory {
+public abstract class BoltFactory {
 
 		public Tej ujTartosTej(long vonalKod, int urtartalom, String gyarto, Date szavatosagiIdo, double zsirtartalom, long ar){
 			return new TartosTej(vonalKod, urtartalom, gyarto, szavatosagiIdo, zsirtartalom, ar);
@@ -14,5 +20,13 @@ public abstract class TejFactory {
 		
 		public Tej ujFeltartosTej(long vonalKod, int urtartalom, String gyarto, Date szavatosagiIdo, double zsirtartalom, long ar){
 			return new FeltartosTej(vonalKod, urtartalom, gyarto, szavatosagiIdo, zsirtartalom, ar);
+		}
+		
+		public Sajt ujSajt(double suly, double zsirtartalom, Long vonalKod, String gyarto, Date szavatossagiIdo){
+			return new Sajt(suly, zsirtartalom, vonalKod, gyarto, szavatossagiIdo);
+		}
+		
+		public Szappan ujSzappan(Long vonalKod, String gyarto, char mosohatas){
+			return new Szappan(vonalKod, gyarto, mosohatas);
 		}
 }
--- src/bolt/aruk/Szappan.java
+++ src/bolt/aruk/Szappan.java
@@ -3,5 +3,15 @@ package bolt.aruk;
 import bolt.Aru;
 
 public class Szappan extends Aru{
+	
+	long vonalKod;
+	String gyarto;
+	char mosohatas;
+	
+	public Szappan(Long vonalKod, String gyarto, char mosohatas){
+		this.vonalKod = vonalKod;
+		this.gyarto = gyarto;
+		this.mosohatas = mosohatas;
+	}
 
 }
